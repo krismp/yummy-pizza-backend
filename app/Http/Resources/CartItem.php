34b6d8b2cd0a78e84263492e1f23a1cd685ad2cb
krismp <kris.mp7777@gmail.com>
2020-05-12 01:15:46 +0700

    Create Order resources

---
+++ app/Http/Resources/CartItem.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CartItem extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'cart_id' => $this->cart_id,
+            'product_id' => $this->product_id,
+            'total' => $this->total,
+            'total_price_in_usd' => $this->total_price_in_usd,
+            'created_at' => $this->created_at->format('d/m/Y'),
+            'updated_at' => $this->updated_at->format('d/m/Y'),
+        ];
+    }
+}
