26903aee825281b928de0be4b3bdb17ec5036c9d
krismp <kris.mp7777@gmail.com>
2020-05-11 22:20:00 +0700

    Create product resources

---
+++ database/migrations/2020_05_11_074401_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->enum('size', ['normal', 'medium', 'large']);
+            $table->float('price_in_usd');
+            $table->bigInteger('stock');
+            $table->text('detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
