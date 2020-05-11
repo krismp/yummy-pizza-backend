055fa894f64ab0181377408d0743c4e8c6908a6b
krismp <kris.mp7777@gmail.com>
2020-05-11 22:21:24 +0700

    Create api routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', 'API\RegisterController@register');
+
+Route::post('login', 'API\RegisterController@login');
+
+Route::resource('products', 'API\ProductController');
+
+Route::middleware('auth:api')->group(function () {
+    
 });
